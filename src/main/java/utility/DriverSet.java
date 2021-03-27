ff5d58974e4df74d6b92e5cf6208d348732e7524
Ruslan Mukashev <rmukashev@mail.ru>
2021-03-27 18:22:33 +0600

    simple driver.get

---
+++ src/main/java/utility/DriverSet.java
@@ -0,0 +1,22 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverSet {
+    private static WebDriver driver;
+    private DriverSet(){
+    }
+    public static WebDriver getDriver(){
+        if(driver==null) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+
+
+
+
+}
