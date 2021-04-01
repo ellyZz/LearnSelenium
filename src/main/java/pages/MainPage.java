ce038d2f3380e1e3518a7e111c558450f6ab9e52
Ruslan Mukashev <rmukashev@mail.ru>
2021-04-02 00:43:30 +0600

    add solutions

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,7 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class MainPage extends BasePage {
@@ -10,35 +14,56 @@ public class MainPage extends BasePage {
     }
 
     private By pasteField = By.id("postform-text");
+    private By syntaxField = By.id("select2-postform-format-container");
+    private By syntaxPushing = By.className("select2-search__field");
     private By pasteExpirationPushing = By.id("select2-postform-expiration-container");
     private By pasteExpirationSelection = By.xpath("//li[text()='10 Minutes']");
     private By pasteNameFieldPushing = By.id("postform-name");
     private By newPasteButton = By.xpath("//button[@class='btn -big']");
-    private By successNote = By.xpath("//div[contains(@class, 'notice -success -post-view')]");
+    private By textFromNameMath = By.xpath("//div[@class='info-top']/child::h1");
+    private By textFromSyntaxButton = By.xpath("//div[@class='left']/child::a");
+    private By textFromDataField = By.xpath("//textarea[@class='textarea']");
 
-    public void openPage() {
+    public void openPage(){
         driver.get("https://pastebin.com");
     }
 
-    public void writeTextInToNewPasteField(String textForNewPaste) {
+    public void writeTextInToNewPasteField(String textForNewPaste){
         driver.findElement(pasteField).sendKeys(textForNewPaste);
     }
 
-    public void selectPasteExpiration() {
+    public void selectSyntaxHighlighting(String syntax){
+        driver.findElement(syntaxField).click();
+        WebElement syntaxSelection = driver.findElement(syntaxPushing);
+        syntaxSelection.sendKeys(syntax);
+        syntaxSelection.sendKeys(Keys.ENTER);
+    }
+    public void selectPasteExpiration(){
         driver.findElement(pasteExpirationPushing).click();
         driver.findElement(pasteExpirationSelection).click();
     }
 
-    public void writeTextInToPasteNameField(String textForPasteName) {
+    public void writeTextInToPasteNameField(String textForPasteName){
         driver.findElement(pasteNameFieldPushing).sendKeys(textForPasteName);
     }
 
-    public void pressCreateNewPasteButton() {
+    public void pressCreateNewPasteButton(){
         driver.findElement(newPasteButton).click();
     }
 
-    public String searchSuccessNote() {
-        return driver.findElement(successNote).getText();
+    public void checkPasteNameMath(String textForPasteName){
+        String isExist = driver.findElement(textFromNameMath).getText();
+        assertEquals(isExist,textForPasteName);
+    }
+
+    public void checkTextOnSyntaxButton(String syntax){
+        String isExist = driver.findElement(textFromSyntaxButton).getText();
+        assertEquals(isExist,syntax);
+    }
+
+    public void checkTextInRawPasteDataField(String textInPasteData){
+        String isExist = driver.findElement(textFromDataField).getText();
+        assertEquals(isExist,textInPasteData);
     }
 }
 
--- src/main/java/tests/MainPageTests.java
+++ src/main/java/tests/MainPageTests.java
@@ -1,17 +1,18 @@
 package tests;
-import org.testng.Assert;
+
+
 import org.testng.annotations.Test;
 
-public class MainPageTests extends BaseTest{
+public class MainPageTests extends BaseTest {
 
     @Test
-    public void createNewPaste(){
+    public void createNewPaste() {
         mainPage.openPage();
         mainPage.writeTextInToNewPasteField("Hello world");
         mainPage.selectPasteExpiration();
         mainPage.writeTextInToPasteNameField("Name");
         mainPage.pressCreateNewPasteButton();
-        Assert.assertEquals(mainPage.searchSuccessNote(), "NOTE: Your guest paste has been posted. If you sign up for a free account, you can edit and delete your pastes!");
+
     }
 }
 
